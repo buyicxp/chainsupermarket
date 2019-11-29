b2657c24545b1efc62db853cd5e22c556c2d4e5d
zuinian <1823579928@qq.com>
2019-11-29 13:44:35 +0800

    百佳华前端

--- src/main.js
+++ src/main.js
@@ -2,8 +2,47 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import Qs from 'qs'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import "babel-polyfill"
 
+Vue.use(ElementUI);
+Vue.prototype.qs = Qs;
+Vue.prototype.$axios = axios
+/* axios.default.baseURL = 'http://localhost:8080' */
+axios.defaults.baseURL = '/api/'  // api 即上面 vue.config.js 中配置的地址
 Vue.config.productionTip = false
+
+
+import {
+	setCookie,
+	getCookie
+} from './assets/cookie.js';
+
+router.beforeEach((to, from, next) => {
+	const roles = localStorage.getItem('roles');
+	const permissions = localStorage.getItem('permissions');
+	//这边可以用match()来判断所有需要权限的路径，to.matched.some(item => return item.meta.loginRequire)
+	let cookieroles = getCookie('roles');
+	if (!cookieroles && to.path !== '/login') { // cookie中有登陆用户信息跳转页面，否则到登陆页面
+		next('/login');
+	} else if (to.meta.permission) { // 如果该页面配置了权限属性（自定义permission）
+		// 如果是管理员权限则可进入
+		roles.indexOf('admin') > -1 ? next() : next('/403');
+	}  else {
+		// 简单的判断IE10及以下不进入富文本编辑器，该组件不兼容
+		if (navigator.userAgent.indexOf('MSIE') > -1 && to.path === '/editor') {
+			Vue.prototype.$alert('vue-quill-editor组件不兼容IE10及以下浏览器，请使用更高版本的浏览器查看', '浏览器不兼容通知', {
+				confirmButtonText: '确定'
+			});
+		} else {
+			next();
+		}
+	}
+})
+
 
 new Vue({
   router,
---
+++ src/views/log/login.vue
@@ -0,0 +1,58 @@
+<template>
+	<div>
+		<h2>登录</h2>
+			<el-form :inline="true" :model="userInfo" class="demo-form-inline">
+				<el-form-item label="用户名">
+					<el-input v-model="userInfo.username" placeholder="用户名"></el-input>
+				</el-form-item><br>
+				<el-form-item label="密码">
+					<el-input v-model="userInfo.password" show-password placeholder="密码"></el-input>
+				</el-form-item>
+			</el-form>
+			<el-button type="primary" @click="submitUser()">确 定</el-button>
+	</div>
+</template>
+
+<script>
+	import {
+		setCookie,
+		getCookie
+	} from '../../assets/cookie.js';
+	export default {
+		data() {
+			return {
+				userInfo: {
+					username: '',
+					password: ''
+				}
+			}
+		},
+		methods: {
+			submitUser() {
+				alert(1);
+				this.$axios.post("/user/login/", this.qs.stringify({
+					'nickname': this.userInfo.username,
+					'password': this.userInfo.password
+				})).then((response) => {
+					var res = response.data.msg;
+					alert(res);
+					if (res == '登录成功') {
+						setCookie('roles', response.data.success.roleList,(24*60*60)); // 使用cookie来记录是否登陆，这边跨域
+						let roles = getCookie('roles');
+						localStorage.setItem("ms_username", this.username); // 使用localstoage来记录登陆信息
+						localStorage.setItem("roles", response.data.success.roleList);
+						localStorage.setItem("permissions", response.data.success.permissionList);
+						this.$router.replace('/main'); //跳转
+					} else {
+						alert('登录失败')
+					}
+				}).catch((error) => {
+					alert(error) //请求失败返回的数据
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>

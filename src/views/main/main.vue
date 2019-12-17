7a7d15e32839d2cbbbaff513f6955ed19341c4ee
ahaoi123 <2991744603@qq. com>
2019-12-17 10:26:52 +0800

    1111

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -17,7 +17,7 @@
 			<el-aside width="27%" style="background-color:white;overflow: hidden;height:1000px;">
 				<el-row class="tac">
 					<el-col :span="12">
-						<el-menu router default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+						<el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
 							<el-submenu index="1">
 								<template slot="title"><i class="el-icon-menu"></i>门店</template>
 								<el-menu-item-group>
@@ -76,10 +76,14 @@
 
 							<el-submenu index="5">
 								<template slot="title"><i class="el-icon-menu"></i>营销</template>
-									<el-menu-item-group>
-									<el-menu-item index="/fu">满减满赠</el-menu-item>
+								<el-menu-item-group>
+									<template slot="title">分组一</template>
+									<el-menu-item index="2-1">选项1</el-menu-item>
+									<el-menu-item index="2-2">选项2</el-menu-item>
 								</el-menu-item-group>
-									<el-menu-item index="/GroupActivities">拼团活动</el-menu-item>
+								<el-menu-item-group title="分组2">
+									<el-menu-item index="2-3">选项3</el-menu-item>
+								</el-menu-item-group>
 								<el-submenu index="2-4">
 									<template slot="title">选项4</template>
 									<el-menu-item index="2-4-1">选项4-1</el-menu-item>
@@ -133,6 +137,11 @@
 									<el-menu-item index="2-4-1">选项4-1</el-menu-item>
 								</el-submenu>
 							</el-submenu>
+
+			<!-- <el-menu-item index="/BooksList" @click="goTo('/BooksList')">
+								<i class="el-icon-menu"></i>
+								<span slot="title">图书管理</span>
+							</el-menu-item> -->
 			</el-menu>
 			</el-col>
 			</el-row>
